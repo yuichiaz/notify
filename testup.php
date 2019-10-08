8361aafb258435570b82d6634cfe02ca79de63aa
yuichiaz <54706250+yuichiaz@users.noreply.github.com>
2019-10-08 15:44:29 +0900

    Update testup.php

--- testup.php
+++ testup.php
@@ -1,28 +1,26 @@
 <?php
-define('LINE_API_URL'  ,'https://notify-api.line.me/api/notify');
-define('LINE_API_TOKEN','nGhjWZFBGjdGCfXxCJRKijEWUWHyanAnBwp3gzmGdBf');
-
+define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
+define('LINE_API_TOKEN','wshV2INzMCHb2nrCe58O5dNgtmiOLNsrOhR0yvIlH4S');    // 「ACCESS_TOKEN」を取得したアクセストークンに置き換える
 function post_message($message){
-
-    $data = http_build_query( [ 'message' => $message ], '', '&');
-
-    $options = [
-        'http'=> [
+    $data = array(
+                        "message" => $message
+                     );
+    $data = http_build_query($data, "", "&");
+    $options = array(
+        'http'=>array(
             'method'=>'POST',
-            'header'=>'Authorization: Bearer ' . LINE_API_TOKEN . "\r\n"
-                    . "Content-Type: application/x-www-form-urlencoded\r\n"
-                    . 'Content-Length: ' . strlen($data)  . "\r\n" ,
-            'content' => $data,
-            ]
-        ];
-
+            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
+                      . "Content-Type: application/x-www-form-urlencoded\r\n"
+                      . "Content-Length: ".strlen($data)  . "\r\n" ,
+            'content' => $data
+        )
+    );
     $context = stream_context_create($options);
-    $resultJson = file_get_contents(LINE_API_URL, false, $context);
-    $resultArray = json_decode($resultJson, true);
-    if($resultArray['status'] != 200)  {
+    $resultJson = file_get_contents(LINE_API_URL,FALSE,$context );
+    $resutlArray = json_decode($resultJson,TRUE);
+    if( $resutlArray['status'] != 200)  {
         return false;
     }
     return true;
 }
-
-post_message('テスト投稿');
+post_message("こんにちわんこ");  // 送信されるメッセージ本文
