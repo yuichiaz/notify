1c63c5ab86e0007a752f40aed42b3276dc0d5b17
yuichiaz <54706250+yuichiaz@users.noreply.github.com>
2019-10-08 15:57:33 +0900

    Update testup.php

--- testup.php
+++ testup.php
@@ -2,19 +2,22 @@
 define('LINE_API_URL'  ,"https://notify-api.line.me/api/notify");
 define('LINE_API_TOKEN','wshV2INzMCHb2nrCe58O5dNgtmiOLNsrOhR0yvIlH4S');    // 「ACCESS_TOKEN」を取得したアクセストークンに置き換える
 function post_message($message){
-    $data = array(
-                        "message" => $message
-                     );
+    $data = array("message" => $message);
     $data = http_build_query($data, "", "&");
-    $options = array(
-        'http'=>array(
-            'method'=>'POST',
-            'header'=>"Authorization: Bearer " . LINE_API_TOKEN . "\r\n"
-                      . "Content-Type: application/x-www-form-urlencoded\r\n"
-                      . "Content-Length: ".strlen($data)  . "\r\n" ,
-            'content' => $data
-        )
-    );
+    
+    
+    $options = array('http'=> array(
+                                'method'=>'POST',
+                                'header'=>"Authorization: Bearer " .
+                                LINE_API_TOKEN . 
+                                "\r\n".
+                                "Content-Type: application/x-www-form-urlencoded\r\n". 
+                                "Content-Length: ".
+                                strlen($data) .
+                                "\r\n" ,'content' => $data
+                              )
+        
+                );
     $context = stream_context_create($options);
     $resultJson = file_get_contents(LINE_API_URL,FALSE,$context );
     $resutlArray = json_decode($resultJson,TRUE);
